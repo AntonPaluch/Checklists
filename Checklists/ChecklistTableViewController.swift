73f438a9c474ada61ca9d0f9975613a561155f78
Dmitry Pavlov <perr07@yandex.ru>
2021-12-01 23:01:33 +0500

    add delet rows

--- Checklists/ChecklistTableViewController.swift
+++ Checklists/ChecklistTableViewController.swift
@@ -97,7 +97,17 @@ class ChecklistTableViewController: UITableViewController {
         label.text = item.text
     }
     
+    //Метод удаления ячеек
+    
+    override func tableView( _ tableView: UITableView,commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath ) {
+        items.remove(at: indexPath.row)
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+    }
+    
+    
 //  MARK: - Actions
+    
     
     @IBAction func addItem() {
         let newRowIndex = items.count
@@ -106,7 +116,7 @@ class ChecklistTableViewController: UITableViewController {
         item.text = "I am a new row"
         item.checked = true
         items.append(item)
-        
+
         let indexPath = IndexPath(row: newRowIndex, section: 0)
         let indexPaths = [indexPath]
         tableView.insertRows(at: indexPaths, with: .automatic)
